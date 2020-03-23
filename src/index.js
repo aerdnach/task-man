d03b7095f44f0e6703159c8bed5ba1baae88f5ac
aerdnach <andreachieppa@gmail.com>
2020-03-23 10:03:40 +0100

    refactoring with async/await"

--- src/index.js
+++ src/index.js
@@ -10,39 +10,36 @@ app.use(express.json())
 
 // USER
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     console.log('POST /users received data' + JSON.stringify(req.body))
     const user = new User(req.body)
     
-    user
-        .save()
-        .then(() => {
-            console.log('POST /users response data: ' + JSON.stringify(user))
-            res.status(201).send(user)
-        })
-        .catch((reason) => {
-            console.log('POST /users error: ' + JSON.stringify(reason))
-            res.status(400).send(reason)
-        })
+    try{
+        await user.save()
+        console.log('POST /users response data: ' + JSON.stringify(user))
+        res.status(201).send(user)
+    } catch (e) {
+        console.log('POST /users error: ' + JSON.stringify(e))
+        res.status(400).send(e)
+    }
+
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
     console.log('GET /users received data' + JSON.stringify(req.body))
-    User
-        .find({}).then((users) => {
-            console.log('GET /users response data' + JSON.stringify(users))
-            res
-                .send(users)
-        })
-        .catch((reason) => {
-            console.log('GET /users error: ' + reason)
-            res
-                .status(500)
-                .send(reason._message)
-        })
+
+    try {
+        const users = await User.find({})
+        console.log('GET /users response data' + JSON.stringify(users))
+        res.send(users)
+    } catch (e) {
+        console.log('GET /users error: ' + e)
+        res.status(500).send(e)
+    }
+
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     console.log('GET /users:id received data ' + JSON.stringify(req.params))
     
     if (!mongoose.isValidMongooseId(req.params.id)) {
@@ -50,64 +47,53 @@ app.get('/users/:id', (req, res) => {
         return res.status(400).send('cannot retrieve user')
     }
     
-    User.
-        findById(req.params.id)
-            .then((user) => {
-                if (!user) {
-                    console.log('GET /users:id cannot retrieve ' + req.params.id)
-                    return res.status(400).send('cannot retrieve user')
-                }
+    try {
+        const user = await User.findById(req.params.id)
 
-                console.log('GET /users response data' + JSON.stringify(user))
-                res.send(user)
-            })
-            .catch((reason) => {
-                console.log('GET /users:id error: ' + reason)
-                res
-                    .status(500)
-                    .send(reason._message)
-            })
+        if (!user) {
+            console.log('GET /users:id cannot retrieve ' + req.params.id)
+            return res.status(400).send('cannot retrieve user')
+        }
+
+        console.log('GET /users response data' + JSON.stringify(user))
+        res.send(user)
+    } catch (e) {
+        console.log('GET /users:id error: ' + e)
+        res.status(500).send(e)
+    }
+    
 })
 
 // TASK 
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     console.log('POST /tasks received data' + JSON.stringify(req.body))
     const task = new Task(req.body)
 
-    task
-        .save()
-        .then(() => {
-            console.log('POST /tasks response data: ' + JSON.stringify(task))
-            res
-                .status(201)
-                .send(task)
-        })
-        .catch((reason) => {
-            console.log('POST /tasks error: ' + reason)
-            res
-                .status(400)
-                .send(reason._message)
-    }) 
+    try {
+        console.log('POST /tasks response data: ' + JSON.stringify(task))
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        console.log('POST /tasks error: ' + reason)
+        res.status(400).send(reason._message)
+    }
 })
 
-app.get('/tasks', (req, res) => {
+app.get('/tasks', async (req, res) => {
     console.log('GET /tasks received data' + JSON.stringify(req.body))
-    Task
-        .find({}).then((tasks) => {
-            console.log('GET /tasks response data' + JSON.stringify(tasks))
-            res
-                .send(tasks)
-        })
-        .catch((reason) => {
-            console.log('GET /tasks error: ' + reason)
-            res
-                .status(500)
-                .send(reason._message)
-        })
+    
+    try {
+        const tasks = await Task.find({})
+        console.log('GET /tasks response data' + JSON.stringify(tasks))
+        res.send(tasks)
+    } catch (e) {
+        console.log('GET /tasks error: ' + e)
+        res.status(500).send(e)
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     console.log('GET /utaskssers:id received data ' + JSON.stringify(req.params))
     
     if (!mongoose.isValidMongooseId(req.params.id)) {
@@ -115,23 +101,20 @@ app.get('/tasks/:id', (req, res) => {
         return res.status(400).send('cannot retrieve task')
     }
     
-    Task.
-        findById(req.params.id)
-            .then((task) => {
-                if (!task) {
-                    console.log('GET /tasks:id cannot retrieve ' + req.params.id)
-                    return res.status(400).send('cannot retrieve task')
-                }
+    try {
+        const task = await Task.findById(req.params.id)
 
-                console.log('GET /tasks response data' + JSON.stringify(task))
-                res.send(task)
-            })
-            .catch((reason) => {
-                console.log('GET /users:id error: ' + reason)
-                res
-                    .status(500)
-                    .send(reason._message)
-            })
+        if (!task) {
+            console.log('GET /tasks:id cannot retrieve ' + req.params.id)
+            return res.status(400).send('cannot retrieve task')
+        }
+
+        console.log('GET /tasks response data' + JSON.stringify(task))
+        res.send(task)
+    } catch (e) {
+        console.log('GET /users:id error: ' + e)
+        res.status(500).send(e)
+    }
 })
 
 // Launch server
