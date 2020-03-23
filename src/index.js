2ac7a6250eb37714205e02d1abc1a264d78ccb5b
aerdnach <andreachieppa@gmail.com>
2020-03-23 16:31:48 +0100

    added patch and delete api

--- src/index.js
+++ src/index.js
@@ -40,10 +40,10 @@ app.get('/users', async (req, res) => {
 })
 
 app.get('/users/:id', async (req, res) => {
-    console.log('GET /users:id received data ' + JSON.stringify(req.params))
+    console.log('GET /users/:id received data ' + JSON.stringify(req.params))
     
     if (!mongoose.isValidMongooseId(req.params.id)) {
-        console.log('GET /users:id cannot retrieve ' + req.params.id)
+        console.log('GET /users/:id cannot retrieve ' + req.params.id)
         return res.status(400).send('cannot retrieve user')
     }
     
@@ -51,17 +51,74 @@ app.get('/users/:id', async (req, res) => {
         const user = await User.findById(req.params.id)
 
         if (!user) {
-            console.log('GET /users:id cannot retrieve ' + req.params.id)
+            console.log('GET /users/:id cannot retrieve ' + req.params.id)
             return res.status(400).send('cannot retrieve user')
         }
 
-        console.log('GET /users response data' + JSON.stringify(user))
+        console.log('GET /users/:id response data' + JSON.stringify(user))
         res.send(user)
     } catch (e) {
-        console.log('GET /users:id error: ' + e)
+        console.log('GET /users/:id error: ' + e)
         res.status(500).send(e)
     }
     
+})
+
+app.patch('/users/:id', async (req, res) => {
+    console.log('PATCH /users/:id ' + JSON.stringify(req.params))
+    
+    try {
+        const requetedUpdates = Object.keys(req.body)
+        const allowedField = ["name", "email", "age", "password"]
+        const isAllowedUpdate = requetedUpdates.every((update) => allowedField.includes(update))
+
+        if (!isAllowedUpdate) {
+            console.log('PATCH /users/:id invalid updates')
+            return res.status(400).send('Invalid Updates')   
+        }
+
+        if (!mongoose.isValidMongooseId(req.params.id)) {
+            console.log('PATCH /users/:id cannot retrieve ' + req.params.id)
+            return res.status(400).send('cannot retrieve user')
+        }
+
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if (!user) {
+            console.log('PATCH /users/:id cannot retrieve ' + req.params.id)
+            return res.status(404).send('User not found')
+        }
+
+        console.log('PATCH /users/:id response data' + JSON.stringify(user))
+        res.send(user)
+    } catch (e) {
+        console.log('PATCH /users/:id error: ' + e)
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    console.log('DELETE /users/:id received data ' + JSON.stringify(req.params))
+    
+    if (!mongoose.isValidMongooseId(req.params.id)) {
+        console.log('DELETE /users/:id cannot retrieve ' + req.params.id)
+        return res.status(400).send('cannot retrieve user')
+    }
+    
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            console.log('DELETE /users/:id cannot retrieve user ' + req.params.id)
+            return res.status(404).send('User not found')
+        }
+        
+        console.log('DELETE /users/:id response data' + JSON.stringify(user))
+        res.send(user)
+    } catch (e) {
+        console.log('DELETE /users/:id error: ' + e)
+        res.status(400).send(e)
+    }
 })
 
 // TASK 
@@ -94,10 +151,12 @@ app.get('/tasks', async (req, res) => {
 })
 
 app.get('/tasks/:id', async (req, res) => {
-    console.log('GET /utaskssers:id received data ' + JSON.stringify(req.params))
+    const requetedUpdates = Object.keys(req.body)
+    const allowedField = ["name", "email", "age", "password"]
+    console.log('GET /tasks/:id received data ' + JSON.stringify(req.params))
     
     if (!mongoose.isValidMongooseId(req.params.id)) {
-        console.log('GET /tasks:id cannot retrieve ' + req.params.id)
+        console.log('GET /tasks/:id cannot retrieve ' + req.params.id)
         return res.status(400).send('cannot retrieve task')
     }
     
@@ -105,15 +164,71 @@ app.get('/tasks/:id', async (req, res) => {
         const task = await Task.findById(req.params.id)
 
         if (!task) {
-            console.log('GET /tasks:id cannot retrieve ' + req.params.id)
+            console.log('GET /tasks/:id cannot retrieve ' + req.params.id)
             return res.status(400).send('cannot retrieve task')
         }
 
-        console.log('GET /tasks response data' + JSON.stringify(task))
+        console.log('GET /tasks/:id response data' + JSON.stringify(task))
         res.send(task)
     } catch (e) {
-        console.log('GET /users:id error: ' + e)
+        console.log('GET /tasks/:id error: ' + e)
         res.status(500).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    console.log('PATCH /tasks/:id received data ' + JSON.stringify(req.params))
+    
+    const requetedUpdates = Object.keys(req.body)
+    const allowedField = ["name", "email", "age", "password"]
+    const isAllowedUpdate = requetedUpdates.every((update) => allowedField.includes(update))
+
+    if (!isAllowedUpdate) {
+        console.log('PATCH /users/:id invalid updates')
+        return res.status(400).send('Invalid Updates')   
+    }
+
+    if (!mongoose.isValidMongooseId(req.params.id)) {
+        console.log('PATCH /tasks/:id cannot retrieve ' + req.params.id)
+        return res.status(400).send('cannot retrieve task')
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if (!task) {
+            return res.status(404).send('Task not found')
+        }
+
+        console.log('PATCH /tasks/:id response data' + JSON.stringify(task))
+        res.send(task)
+    } catch (e) {
+        console.log('PATCH /tasks/:id error: ' + e)
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    console.log('DELETE /tasks/:id received data ' + JSON.stringify(req.params))
+    
+    if (!mongoose.isValidMongooseId(req.params.id)) {
+        console.log('DELETE /tasks/:id cannot retrieve ' + req.params.id)
+        return res.status(400).send('cannot retrieve task')
+    }
+    
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            console.log('DELETE /tasks/:id cannot retrieve user ' + req.params.id)
+            return res.status(404).send('Task not found')
+        }
+        
+        console.log('DELETE /tasks/:id response data' + JSON.stringify(task))
+        res.send(task)
+    } catch (e) {
+        console.log('DELETE /tasks/:id error: ' + e)
+        res.status(400).send(e)
     }
 })
 
